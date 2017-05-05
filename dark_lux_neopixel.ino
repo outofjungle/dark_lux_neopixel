3d0a737e54e8311bf4592ba9a0e354d1c93a4c29
Venkat Venkataraju <ven@yubico.com>
2017-05-05 00:20:07 -0700

    cleanup

--- dark_lux_neopixel.ino
+++ dark_lux_neopixel.ino
@@ -10,10 +10,13 @@
 #define RED 255
 #define MAX_GREEN 50
 
-Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(30, PIN1, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(30, PIN2, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strips [] = {
+  Adafruit_NeoPixel(30, PIN1, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(30, PIN2, NEO_GRB + NEO_KHZ800)
+};
 
-uint8_t i;
+
+uint8_t i, j;
 uint32_t pixelColor;
 
 void setup() {
@@ -23,38 +26,36 @@ void setup() {
 
   uint8_t g;
   randomSeed(analogRead(0));
-  
-  strip1.begin();
-  strip2.begin();
-  
-  for(i=0; i< strip1.numPixels(); i++) {
-    g = random(MAX_GREEN);
-    strip1.setPixelColor(i, strip1.Color(RED, g, BLUE+1));
+
+  for(i=0; i<2; i++) {
+    strips[i].begin();
   }
 
-  for(i=0; i< strip2.numPixels(); i++) {
-    g = random(MAX_GREEN);
-    strip2.setPixelColor(i, strip2.Color(RED, g, BLUE+1));
+  for(i=0; i<2; i++) {
+    for(j=0; j< strips[i].numPixels(); j++) {
+      g = random(MAX_GREEN);
+      strips[i].setPixelColor(j, strips[i].Color(RED, g, BLUE+1));
+    }
   }
-
-  strip1.show(); 
-  strip2.show();
+  
+  for(i=0; i<2; i++) {
+    strips[i].show();
+  }
 }
 
 void loop() {
-  for(i=0; i< strip1.numPixels(); i++) {
-    pixelColor = purpleBeatG(strip1, i);
-    strip1.setPixelColor(i, pixelColor);
+  for(i=0; i<2; i++) {
+    for(j=0; j< strips[i].numPixels(); j++) {
+      pixelColor = purpleBeatG(strips[i], j);
+      strips[i].setPixelColor(j, pixelColor);
+    }
   }
-
-  for(i=0; i< strip2.numPixels(); i++) {
-    pixelColor = purpleBeatG(strip2, i);
-    strip2.setPixelColor(i, pixelColor);
+  
+  for(i=0; i<2; i++) {
+    strips[i].show();
   }
-
-  strip1.show(); 
-  strip2.show();
-  delay(4);
+  
+  delay(2);
 }
 
 uint32_t purpleBeatG(Adafruit_NeoPixel &strip, uint16_t i) {
